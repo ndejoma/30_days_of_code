db7efa7c0bc2f7433a0c153326c206e9b8d93ac6
ndejoma <ndegwaj@proton.me>
2023-07-24 08:33:05 +0300

    30 days of code challenge: day 11: 2D arrays

---
+++ day_12/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	var arr [][]int32
+	for i := 0; i < 6; i++ {
+		arrRowTemp := strings.Split(strings.TrimRight(readLine(reader), " \t\r\n"), " ")
+		var arrRow []int32
+		for _, arrRowItem := range arrRowTemp {
+			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+			checkError(err)
+			arrItem := int32(arrItemTemp)
+			arrRow = append(arrRow, arrItem)
+		}
+
+		if len(arrRow) != 6 {
+			panic("Bad input")
+		}
+
+		arr = append(arr, arrRow)
+	}
+	var max int32 = -1000
+	for i := 0; i < len(arr)-2; i++ {
+		// var sum int32
+		for j := 0; j < len(arr[0])-2; j++ {
+			sum := arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+			if sum > max {
+				max = sum
+			}
+		}
+	}
+	fmt.Println(max)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
