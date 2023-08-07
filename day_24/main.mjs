ab96d9cb1dbc0dd8edf17e95300a7e00f0a6ca3f
ndejoma <ndegwaj@proton.me>
2023-08-07 09:45:34 +0300

    30 days of code challenge: day 24: More Linked lists

---
+++ day_24/main.mjs
@@ -0,0 +1,78 @@
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+
+var input_stdin = ''
+var input_stdin_array = ''
+var input_currentline = 0
+
+process.stdin.on('data', function (data) {
+  input_stdin += data
+})
+
+process.stdin.on('end', function () {
+  input_stdin_array = input_stdin.split('\n')
+  main()
+})
+function readLine() {
+  return input_stdin_array[input_currentline++]
+}
+function Node(data) {
+  this.data = data
+  this.next = null
+}
+function Solution() {
+  this.removeDuplicates = function (head) {
+    //Write your code here
+    const mySet = new Set()
+    if (head === null) {
+      return head
+    }
+    //add the next element to the set
+    mySet.add(head.data)
+    let curr = head
+    while (curr.next !== null) {
+      if (mySet.has(curr.next.data)) {
+        curr.next = curr.next.next
+      } else {
+        mySet.add(curr.next.data)
+        curr = curr.next
+      }
+    }
+    return head
+  }
+
+  this.insert = function (head, data) {
+    var p = new Node(data)
+    if (head == null) {
+      head = p
+    } else if (head.next == null) {
+      head.next = p
+    } else {
+      var start = head
+      while (start.next != null) {
+        start = start.next
+      }
+      start.next = p
+    }
+    return head
+  }
+
+  this.display = function (head) {
+    var start = head
+    while (start) {
+      process.stdout.write(start.data + ' ')
+      start = start.next
+    }
+  }
+}
+function main() {
+  var T = parseInt(readLine())
+  var head = null
+  var mylist = new Solution()
+  for (let i = 0; i < T; i++) {
+    var data = parseInt(readLine())
+    head = mylist.insert(head, data)
+  }
+  head = mylist.removeDuplicates(head)
+  mylist.display(head)
+}
